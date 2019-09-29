c565d6f38cbd20cc6013dd02db18f228fddba6a4
Yurii Haideichuk <yuriy.haydeychuk@gmail.com>
2019-09-30 01:05:50 +0300

    db

---
+++ createDB.sql
@@ -0,0 +1,12 @@
+-- Database: chat
+
+-- DROP DATABASE chat;
+
+CREATE DATABASE chat
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Ukrainian_Ukraine.1251'
+    LC_CTYPE = 'Ukrainian_Ukraine.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
